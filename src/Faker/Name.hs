262443a0c67d15d403063236d7a5f6841428da2c
Alexey Gaziev <alex.gaziev@gmail.com>
2014-12-15 20:53:08 +0800

    Working first/last/full name functions

--- src/Faker/Name.hs
+++ src/Faker/Name.hs
@@ -1,8 +1,11 @@
 module Faker.Name
 (
-  firstNames
---, lastName
---, fullName
+  firstName
+, lastName
+, fullName
+, namesList
+, firstNames
+, name
 )
 where
 
@@ -10,44 +13,51 @@ import System.Random
 import Control.Applicative
 import Control.Monad
 import Data.Maybe
-import Gimlh
+import Gimlh -- need to renew
+
+fetch :: SimpleGiml -> String -> Maybe GimlVal
+fetch [] _ = Nothing
+fetch ((key, val):xs) req = if key == req
+                              then return val
+                              else fetch xs req
+
+val2List :: GimlVal -> [String]
+val2List (List val)   = val
+val2List (Text val)   = [val]
+val2List (Number val) = [show val]
+val2List (Float val)  = [show val]
 
 namesList :: String -> IO [String]
 namesList namesType = do
-    contents <- simplifyGiml $ parseFile "../data/en.giml"
+    contents <- parseFile "../data/en.giml"
+    let fetchedVal = fetch (simplifyGiml contents) namesType
+    case fetchedVal of
+      Nothing    -> return []
+      (Just val) -> return $ val2List val
 
+firstNames :: IO [String]
+firstNames = namesList "name$first_name"
 
-firstNames' :: IO ()
-firstNames' = do
-    content <- BS.readFile "../data/en.yml"
-    let names = Data.Yaml.decode content :: Maybe [FirstNames]
-        lgth = length $ fromJust names
-    print $ show lgth
-    print $ fromJust names
+lastNames :: IO [String]
+lastNames = namesList "name$last_name"
 
-lastNames :: [String]
-lastNames = [ "Gaziev"
-            , "Kapkov"
-            , "Black"
-            ]
+name :: String -> IO String
+name nameType = do
+    gen <- newStdGen
+    names <- case nameType of
+               "first"   -> firstNames
+               otherwise -> lastNames
+    let ind = fst $ randomR (0, length names - 1) gen
+    return $ names !! ind
 
--- name :: String -> IO String
--- name nameType = do
---     gen <- newStdGen
---     let names = case nameType of
---                   "first"   -> firstNames
---                   otherwise -> lastNames
---         ind = fst $ randomR (0, length names - 1) gen
---     return $ names !! ind
+firstName :: IO String
+firstName = name "first"
 
--- firstName :: IO String
--- firstName = name "first"
+lastName :: IO String
+lastName = name "last"
 
--- lastName :: IO String
--- lastName = name "last"
-
--- fullName :: IO String
--- fullName = do
---     first <- name "first"
---     last  <- name "last"
---     return $ first ++ " " ++ last
+fullName :: IO String
+fullName = do
+    first <- name "first"
+    last  <- name "last"
+    return $ first ++ " " ++ last
