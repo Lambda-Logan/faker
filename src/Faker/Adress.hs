30aff227c6e779ae095ed8efc576057a9c852c23
Alexey Gaziev <alex.gaziev@gmail.com>
2015-01-03 15:34:34 +0800

    Start address

--- src/Faker/Adress.hs
+++ src/Faker/Adress.hs
@@ -15,3 +15,38 @@ module Faker.Adress
 , defaultCountry
 )
 where
+
+import Faker.Fetcher
+
+cityPrefix :: IO String
+cityPrefix = randomAddress "city_prefix"
+
+citySuffix :: IO String
+citySuffix = randomAddress "city_suffix"
+
+country :: IO String
+country = randomAddress "country"
+
+countryCode :: IO String
+countryCode = randomAddress "country_code"
+
+countryCode :: IO String
+countryCode = randomAddress "country_code"
+
+buildingNumber :: IO String
+buildingNumber = randomAddress "building_number"
+
+streetSuffix :: IO String
+streetSuffix = randomAddress "street_suffix"
+
+secondaryAddress :: IO String
+secondaryAddress = randomAddress "secondary_address"
+
+postcode :: IO String
+postcode = randomAddress "postcode"
+
+postcodeByState :: IO String
+postcodeByState = randomAddress "postcode_by_state"
+
+randomAddress :: String -> IO String
+randomAddress attr = randomValue "address" attr
