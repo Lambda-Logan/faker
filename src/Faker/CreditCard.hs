2ad89e6265b6011b5663aeaaaa2c571ea5b91313
Alexey Gaziev <alex.gaziev@gmail.com>
2015-01-03 22:50:57 +0800

    Credit card with luhn sum

--- src/Faker/CreditCard.hs
+++ src/Faker/CreditCard.hs
@@ -15,6 +15,7 @@ module Faker.CreditCard
 )
 where
 
+import Data.Char (digitToInt, isDigit)
 import Faker.Utils
 
 visa :: IO String
@@ -54,4 +55,24 @@ laser :: IO String
 laser = randomCardNumber "laser"
 
 randomCardNumber :: String -> IO String
-randomCardNumber attr = randomValue "credit_card" attr >>= evalRegex
+randomCardNumber attr = randomValue "credit_card" attr >>= evalRegex >>= addLuhnSum
+
+addLuhnSum :: String -> IO String
+addLuhnSum numberString = do
+    let numbers = collectNumbers numberString
+        luhnSum = countLuhnSum numbers 2
+        luhnDigit = (10 - (luhnSum `mod` 10)) `mod` 10
+    return $ (init numberString) ++ (show luhnDigit)
+
+countLuhnSum :: [Int] -> Int -> Int
+countLuhnSum [] _ = 0
+countLuhnSum (x:xs) m = let nextM = if m == 2 then 1 else 2 in
+                            (luhnStep x m) + (countLuhnSum xs nextM)
+
+luhnStep :: Int -> Int -> Int
+luhnStep x m = sum $ map digitToInt (show (x * m))
+
+collectNumbers :: String -> [Int]
+collectNumbers [] = []
+collectNumbers str = foldl (\a x -> if isDigit x then (digitToInt x) : a else a) [] str
+
