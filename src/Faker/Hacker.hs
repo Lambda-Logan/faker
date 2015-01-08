a54ccac0448b07678512a87a0e952ee9dcb56303
Alexey Gaziev <alex.gaziev@gmail.com>
2015-01-08 23:21:37 +0800

    Starting Hacker

---
+++ src/Faker/Hacker.hs
@@ -0,0 +1,62 @@
+module Faker.Hacker
+(
+  saySomethingSmart
+, abbreviation
+, adjective
+, noun
+, verb
+, ingverb
+, phrases
+)
+where
+
+import Faker.Utils
+
+saySomethingSmart :: Faker String
+saySomethingSmart = do
+    ind <- randomInt (0, length phrases - 1)
+    evalPhrase $ phrases !! ind
+
+abbreviation :: Faker String
+abbreviation = randomHackerWord "abbreviation"
+
+adjective :: Faker String
+adjective = randomHackerWord "adjective"
+
+noun :: Faker String
+noun = randomHackerWord "noun"
+
+verb :: Faker String
+verb = randomHackerWord "verb"
+
+ingverb :: Faker String
+ingverb = randomHackerWord "ingverb"
+
+phrases :: [String]
+phrases = [ "If we #{verb} the #{noun}, we can get to the #{abbreviation} #{noun} through the #{adjective} #{abbreviation} #{noun}!"
+          , "We need to #{verb} the #{adjective} #{abbreviation} #{noun}!"
+          , "Try to #{verb} the #{abbreviation} #{noun}, maybe it will #{verb} the #{adjective} #{noun}!"
+          , "You can't #{verb} the #{noun} without #{ingverb} the #{adjective} #{abbreviation} #{noun}!"
+          , "Use the #{adjective} #{abbreviation} #{noun}, then you can #{verb} the #{adjective} #{noun}!"
+          , "The #{abbreviation} #{noun} is down, #{verb} the #{adjective} #{noun} so we can #{verb} the #{abbreviation} #{noun}!"
+          , "#{ingverb} the #{noun} won't do anything, we need to #{verb} the #{adjective} #{abbreviation} #{noun}!"
+          , "I'll #{verb} the #{adjective} #{abbreviation} #{noun}, that should #{noun} the #{abbreviation} #{noun}!"
+          ]
+
+evalPhrase :: String -> Faker String
+evalPhrase [] = return ""
+evalPhrase phrase = return $ unwords $ map evalWord (words phrase)
+
+evalWord :: String -> Faker String
+evalWord [] = return ""
+evalWord ('#':word) = do
+    case init $ tail word of
+      "abbreviation" -> abbreviation
+      "adjective"    -> adjective
+      "noun"         -> noun
+      "verb"         -> verb
+      _              -> ingverb
+evalWord word = word
+
+randomHackerWord :: String -> Faker String
+randomHackerWord = randomValue "hacker"
