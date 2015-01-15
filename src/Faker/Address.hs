586cc37200cee671639be0949d3d6f304f8ba87c
Alexey Gaziev <alex.gaziev@gmail.com>
2015-01-16 03:57:25 +0800

    Finished Faker.Address documentation

--- src/Faker/Address.hs
+++ src/Faker/Address.hs
@@ -37,7 +37,7 @@ where
 import Faker.Utils
 import qualified Faker.Name as N
 
--- | Returns fake city name, wrapped in 'Faker' monad, i.e. Faker "
+-- | Returns random city name, i.e. "West Felicitymouth"
 city :: Faker String
 city = do
     fName <- N.firstName
@@ -51,6 +51,7 @@ city = do
                2 -> fName ++ suff
                _ -> lName ++ suff
 
+-- | Returns random street name, i.e. "Alfreda Fall"
 streetName :: Faker String
 streetName = do
     fName <- N.firstName
@@ -61,58 +62,78 @@ streetName = do
                0 -> fName ++ " " ++ suff
                _ -> lName ++ " " ++ suff
 
+-- | Returns random street address, i.e. "78268 McDermott Run"
 streetAddress :: Faker String
 streetAddress = do
     sName <- streetName
     bNum  <- buildingNumber
     return $ bNum ++ " " ++ sName
 
+-- | Returns random latitude, i.e. "21.599999999999994"
 latitude :: Faker String
 latitude = do
     num <- randomInt (1,99)
     return . show $ ((fromIntegral num :: Double) / 100) * 180 - 90
 
+-- | Returns random longitude, i.e. "-97.2"
 longitude :: Faker String
 longitude = do
     num <- randomInt (1,99)
     return . show $ ((fromIntegral num :: Double) / 100) * 360 - 180
 
+-- | Part of 'Faker.Address.city' function Returns random prefix for city
+-- name, i.e. "Port"
 cityPrefix :: Faker String
 cityPrefix = randomAddress "city_prefix"
 
+-- | Part of 'Faker.Address.city' function Returns random suffix for city
+-- name, i.e. "borough"
 citySuffix :: Faker String
 citySuffix = randomAddress "city_suffix"
 
+-- | Returns random country name, i.e. "Ecuador"
 country :: Faker String
 country = randomAddress "country"
 
+-- | Returns random country code, i.e. "MT"
 countryCode :: Faker String
 countryCode = randomAddress "country_code"
 
+-- | Part of 'Faker.Address.streetAddress' function.
+-- Returns random building number, i.e. "97627"
 buildingNumber :: Faker String
 buildingNumber = randomAddress "building_number" >>= replaceSymbols
 
+-- | Part of 'Faker.Address.streetName' function.
+-- Returns random street suffix, i.e. "Manors"
 streetSuffix :: Faker String
 streetSuffix = randomAddress "street_suffix"
 
+-- | Returns random secondary address, i.e. "Suite 242"
 secondaryAddress :: Faker String
 secondaryAddress = randomAddress "secondary_address" >>= replaceSymbols
 
+-- | Returns random postcode, i.e. "87695"
 postcode :: Faker String
 postcode = randomAddress "postcode" >>= replaceSymbols
 
+-- | Returns random postcode by state, i.e. "55790-3844"
 postcodeByState :: Faker String
 postcodeByState = randomAddress "postcode_by_state" >>= replaceSymbols
 
+-- | Returns random US state name, i.e. "Pennsylvania"
 state :: Faker String
 state = randomAddress "state"
 
+-- | Returns random US state's abbriveation, i.e. "OK"
 stateAbbr :: Faker String
 stateAbbr = randomAddress "state_abbr"
 
+-- | Returns random timezone, i.e. "Africa/Harare"
 timeZone :: Faker String
 timeZone = randomAddress "time_zone"
 
+-- | Returns default country for current locale, i.e. "United States of America"
 defaultCountry :: Faker String
 defaultCountry = randomAddress "default_country"
 
