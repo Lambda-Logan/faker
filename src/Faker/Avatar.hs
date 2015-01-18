a874596e2f7155558b19497a143973daf0d4db32
Alexey Gaziev <alex.gaziev@gmail.com>
2015-01-18 22:16:05 +0800

    Avatar docs

--- src/Faker/Avatar.hs
+++ src/Faker/Avatar.hs
@@ -1,5 +1,17 @@
+{-|
+Module        : Faker.Avatar
+Description   : Module for generating fake avatar url
+Copyright     : (c) Alexey Gaziev, 2015
+License       : MIT
+Maintainer    : alex.gaziev@gmail.com
+Stability     : experimental
+Portability   : POSIX
+
+Fake data
+-}
 module Faker.Avatar
 (
+-- * Function for generate fake avatar url
   image
 )
 where
@@ -7,6 +19,8 @@ where
 import Faker.Utils
 import qualified Faker.Lorem as L
 
+-- | Returns random avatar url from robohash.org with 300x300 size,
+-- i.e. "http://robohash.org/suntbestia.png?size=300x300"
 image :: Faker String
 image = do
     ws <- randomInt(2,5) >>= L.words
