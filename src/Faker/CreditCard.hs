2b68ded23cc828a9cc788649d2c1fde62c94d864
Alexey Gaziev <alex.gaziev@gmail.com>
2015-01-20 22:02:11 +0800

    Credit card docs and fix for diners club card

--- src/Faker/CreditCard.hs
+++ src/Faker/CreditCard.hs
@@ -1,5 +1,17 @@
+{-|
+Module        : Faker.App
+Description   : Module for generating fake credit card numbers
+Copyright     : (c) Alexey Gaziev, 2015
+License       : MIT
+Maintainer    : alex.gaziev@gmail.com
+Stability     : experimental
+Portability   : POSIX
+
+Fake data
+-}
 module Faker.CreditCard
 (
+-- * Functions for generate fake credit card numbers
   visa
 , mastercard
 , discover
@@ -18,20 +30,25 @@ where
 import Data.Char (digitToInt, isDigit)
 import Faker.Utils
 
+-- | Returns random visa card number, i.e. "4784066907150"
 visa :: Faker String
 visa = randomCardNumber "visa"
 
+-- | Returns random mastercard card number, i.e. "5524-7275-2305-9123"
 mastercard :: Faker String
 mastercard = randomCardNumber "mastercard"
 
+-- | Returns random discover card number, i.e. "6485-6297-9249-9908-4511"
 discover :: Faker String
 discover = randomCardNumber "discover"
 
+-- | Returns random discover card number, i.e. "3772-746109-17862"
 americanExpress :: Faker String
 americanExpress = randomCardNumber "american_express"
 
+-- | Returns random discover card number, i.e. "3772-746109-17862"
 dinersClub :: Faker String
-dinersClub = randomCardNumber "dinersClub"
+dinersClub = randomCardNumber "diners_club"
 
 jcb :: Faker String
 jcb = randomCardNumber "jcb"
